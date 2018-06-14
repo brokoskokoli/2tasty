d58be9cc4ac74a09eb589ce73f91b8a4f5a6d022
brokoskokoli <brokoskokoli@gmail.com>
2018-06-14 16:32:29 +0200

    correction bugs ui

--- assets/js/initialize.js
+++ assets/js/initialize.js
@@ -7,7 +7,7 @@ require('assets/js/base/user.js');
 require('assets/js/3rdParty/10-jquery.instantSearch.js');
 require('assets/js/base/search.js');
 
-$('option').mousedown(function(e) {
+$('select.bettermultiple option').mousedown(function(e) {
     e.preventDefault();
     $(this).prop('selected', !$(this).prop('selected'));
     return false;
