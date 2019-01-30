a3ea808b793e2dfe6de29e925d7b601ffb89ddb6
brokoskokoli <brokoskokoli@gmail.com>
2019-01-30 14:51:57 +0100

    fix user create

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -201,7 +201,6 @@ class User implements UserInterface, \Serializable
     {
         $this->setUpdatedAt(new \DateTime());
         $this->altText = '';
-        $this->ingredientDisplayPreference = self::DISPLAY_PREFERENCE_NATIVE;
         $this->ingredientDisplayPreferenceOverrides = new ArrayCollection();
     }
 
